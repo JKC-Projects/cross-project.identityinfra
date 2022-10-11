a3df12aa12f3c9efa9836cdd9978c417bda078bc
JK-Chung <chungjeeken@outlook.com>
2022-10-11 18:39:22 +0100

    SDOMAINS-9 adding skeleton project

---
+++ provider.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.8"
+    }
+  }
+
+  # set TF CLI version
+  required_version = "~> 1.1"
+
+  # use Terraform Cloud as state backend
+  cloud {
+    organization = "jkc-projects"
+    workspaces {
+      tags = ["identityinfra"]
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    project     = "smalldomains"
+    managed_by  = "terraform"
+    github_repo = "smalldomains.identityinfra"
+  }
+}
+
+data "aws_region" "current" {}
