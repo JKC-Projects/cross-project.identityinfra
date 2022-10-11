e09c537c43610d6e8a036b0c579fd96c012ff77a
JK-Chung <chungjeeken@outlook.com>
2022-10-11 21:45:39 +0100

    SDOMAINS-9 creating a custom domain for the cognito user pool

--- route53.tf
+++ route53.tf
@@ -2,7 +2,6 @@ resource "aws_route53_record" "smalldomains_auth" {
   allow_overwrite = true
   name            = aws_cognito_user_pool_domain.smalldomains.domain
   type            = "A"
-  ttl             = 60
   zone_id         = data.aws_ssm_parameter.smalldomains_zone_id
 
   alias {
