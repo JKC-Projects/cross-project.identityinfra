25e6a1757af3803fd9165b7afce594aa9134746b
JK-Chung <chungjeeken@outlook.com>
2022-10-11 21:42:03 +0100

    SDOMAINS-9 creating a custom domain for the cognito user pool

--- acm.tf
+++ acm.tf
@@ -4,6 +4,6 @@ module "auth" {
   }
 
   source          = "./validated_tls_cert"
-  fqdn            = aws_cognito_user_pool_domain.smalldomains.domain
+  fqdn            = local.fqdn
   route53_zone_id = data.aws_ssm_parameter.smalldomains_zone_id.value
 }
