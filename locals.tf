102c03c1f89cb2a5071c8041c8ea446767b747e2
JK-Chung <chungjeeken@outlook.com>
2022-10-15 11:56:43 +0100

    SDOMAINS-9 Shortening FQDN of hosted UI

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   env_root_domain = format("%sjohn-chung.dev", var.environment == "prod" ? "" : "${var.environment}.")
-  fqdn            = format("authentication.%s", local.env_root_domain)
+  fqdn            = format("auth.%s", local.env_root_domain)
 
   default_tags = {
     project     = "cross-project"
