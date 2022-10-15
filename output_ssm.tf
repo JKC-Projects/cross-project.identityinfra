1d0d38e2759f9fe16cb77feb25d163a00bde49cd
JK-Chung <chungjeeken@outlook.com>
2022-10-15 13:40:13 +0100

    SDOMAINS-9 correcting terraform

--- output_ssm.tf
+++ output_ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "client_ids_auth_only" {
-  for_each    = { for c in local.user_pool_clients_auth_only : c.name => c }
+  for_each    = { for c in local.user_pool_clients_auth_only : c.client_name => c }
   name        = "/cognito/${local.userpool_name}/${each.value.project}/${each.value.application}/client-id"
   type        = "String"
   value       = aws_cognito_user_pool_client.auth_only[each.value.client_name].id
